f8bfcd7e58c3f611dba80c8b9e43bc104742685f
Bill Barnes <billba@microsoft.com>
2024-05-22 10:44:55 -0700

    muuuuuuch better algorithn

--- src/selection.test.ts
+++ src/selection.test.ts
@@ -4,30 +4,35 @@ import { search } from './selection'
 const searchTest = (
   searchIn: string,
   searchFor: string,
-  anchorId: number,
-  anchorOffset: number,
-  focusId: number,
-  focusOffset: number
+  expected: [anchorId: number, anchorOffset: number, focusId: number, focusOffset: number][],
 ) => test(`searches |${searchIn}| for |${searchFor}|`, () => {
   const root = document.createElement('div');
   root.innerHTML = searchIn.split('|').map((s, i) => `<span id='${i}'>${s}</span>`).join('');
-  const results = search(searchFor, root);
-  expect(results?.length).toBe(1);
-  const { anchor, focus } = results![0];
-  expect(Number(anchor.spanNode.id)).toBe(anchorId);
-  expect(anchor.offset).toBe(anchorOffset);
-  expect(Number(focus.spanNode.id)).toBe(focusId);
-  expect(focus.offset).toBe(focusOffset);
+  const actual = [...search(searchFor, root)];
+  expect(actual).toHaveLength(expected.length);
+  for (let i = 0; i < expected.length; i++) {
+    const { anchor, focus } = actual[i];
+    const [anchorId, anchorOffset, focusId, focusOffset] = expected[i];
+    expect(Number(anchor.span.id)).toBe(anchorId);
+    expect(anchor.offset).toBe(anchorOffset);
+    expect(Number(focus.span.id)).toBe(focusId);
+    expect(focus.offset).toBe(focusOffset);
+  }
 });
 
-searchTest('a b c|d e', 'b c', 0, 2, 0, 5);
-searchTest('a  b c|d e f', 'b c', 0, 3, 0, 6);
-searchTest('a b c|d e f', 'c d', 0, 4, 1, 1);
-searchTest('a b c|d e f', 'd', 1, 0, 1, 1);
-searchTest('a b c|d e f', 'b  c', 0, 2, 0, 5);
-searchTest('a b b c|d e f', 'b c', 0, 4, 0, 7);
-searchTest('a b c|d e f', 'b c d', 0, 2, 1, 1);
-searchTest('a b c |d e f', 'b c', 0, 2, 0, 5);
-searchTest('a b c |d e f', 'b c d', 0, 2, 1, 1);
-searchTest('a b c| d e f', 'b c d', 0, 2, 1, 2);
-searchTest('a b c | d e f', 'b c d', 0, 2, 1, 2);
+searchTest('a b c|d e', 'b c', [[0, 2, 0, 5]]);
+searchTest('a  b c|d e f', 'b c', [[0, 3, 0, 6]]);
+searchTest('a b c|d e f', 'c d', [[0, 4, 1, 1]]);
+searchTest('a b c|d e f', 'd', [[1, 0, 1, 1]]);
+searchTest('a b c|d e f', 'b  c', [[0, 2, 0, 5]]);
+searchTest('a b b c|d e f', 'b c', [[0, 4, 0, 7]]);
+searchTest('a b c|d e f', 'b c d', [[0, 2, 1, 1]]);
+searchTest('a b c |d e f', 'b c', [[0, 2, 0, 5]]);
+searchTest('a b c |d e f', 'b c d', [[0, 2, 1, 1]]);
+searchTest('a b c| d e f', 'b c d', [[0, 2, 1, 2]]);
+searchTest('a b c | d e f', 'b c d', [[0, 2, 1, 2]]);
+searchTest('a b c||d e f', 'c d', [[0, 4, 2, 1]]);
+searchTest('a b c b c d', 'b c', [[0, 2, 0, 5], [0, 6, 0, 9]]);
+searchTest('a b c|b c d', 'b c', [[0, 2, 0, 5], [1, 0, 1, 3]]);
+searchTest('a b|c b c d', 'b c', [[0, 2, 1, 1],[1, 2, 1, 5]]);
+// searchTest('a b c b c b', 'b c b', [[0, 2, 0, 7],[0, 6, 0, 11]]); // this fails because matchAll won't find overlaps
--- src/selection.ts
+++ src/selection.ts
@@ -13,107 +13,66 @@
   via Form Recognizer or perhaps a Python package like PyMuPDF. So we have an impedance
   mismatch between the two representations of the text.
 
-  To overcome this, we normalize the source text by concatanating all the non-empty spans of text
-  into a single string, adding spaces between each span, converting all runs of spaces
-  into a single space, and trimming spaces from the beginning and end of the string:
-  
-    the source text is divided into spans of text like this with varying spacing and line breaks
- 
-  We similarly normalize the source text by removing all line breaks and removing extra spaces:
-  
-    text like this with varying spacing and
-  
-  Now we can easily search for the text in the normalized source text.
-  
-  Once we have the location of the text we just iterate through the spans to find the
-  start ("anchor") and end ("focus") spans. Because we have messed with whitespace,
-  we need to fuss just a little bit to find the precise offsets within the spans.
+  To do this we join the texts of all the non-empty spans, with a line break in between:
+     the source text is
+    divided into
+    spans of  text
+    like this
+     with  varying spacing
+    and line breaks
+
+  Then we turn the search string into a regular expression that is flexible about whitespace.
+
+
 */
 
-interface SelectionPart {
-  spanNode: HTMLSpanElement;
-  textNode: Text;
-  offset: number;
-  text: string;
-}
-
-export function search(searchText: string, root: HTMLDivElement) {
+export function* search(searchText: string, root: HTMLDivElement)  {
   const spans = [...root.childNodes].filter(node => node instanceof HTMLSpanElement && node.innerText.length > 0) as HTMLSpanElement[];
-  
-  const normalizedSourceText = normalizedText(spans.map(span => span.innerText).join(' '));
-  const normalizedSearchText = normalizedText(searchText);
+  const pageTexts = spans.map(span => span.innerText);
 
-  // right now let's just find the first instance of the text
-  const location = normalizedSourceText.indexOf(normalizedSearchText);
-  
-  if (location == -1) {
-    return;
-  }
+  let i = 0;
+  const pageTextRanges = pageTexts.map(text => [i, i += text.length]);
 
-  let anchor: SelectionPart | undefined = undefined;
-  let focus: SelectionPart | undefined = undefined;
- 
-  let index = 0;
+  const pageText = pageTexts.join('\n');
 
-  for (const spanNode of spans) {
-    const textNode = spanNode.firstChild as Text;
-    const innerText = normalizedText(textNode.textContent!);
+  const searchTextRegExp = new RegExp(escape(searchText).replace(/\s+/g, '\\s+'), 'g');
+  const matches = [...pageText.matchAll(searchTextRegExp)];
 
-    if (!anchor && index + innerText.length > location) {
-      const offset = location - index;
-      anchor = {
-        spanNode,
-        textNode,
-        offset,
-        text: innerText.substring(offset),
-      }
+  for (const match of matches) {
+    const index = match.index!;
+    const length = match[0].length;
+
+    // if the below ever became a performance bottleneck, we could use a binary search
+    const anchorIndex = pageTextRanges.findIndex(([start, end]) => start <= index && index < end);
+    const focusIndex = pageTextRanges.findIndex(([start, end]) => start < index + length && index + length <= end);
+
+    yield {
+      anchor: {
+        span: spans[anchorIndex],
+        text: spans[anchorIndex].firstChild as Text,
+        offset: index - pageTextRanges[anchorIndex][0] - anchorIndex,
+      },
+      focus: {
+        span: spans[focusIndex],
+        text: spans[focusIndex].firstChild as Text,
+        offset: index + length - pageTextRanges[focusIndex][0] - focusIndex,
+      },
     }
-
-    if (anchor && index + innerText.length >= location + normalizedSearchText.length) {
-      const offset = location + normalizedSearchText.length - index;
-      focus = {
-        spanNode,
-        textNode,
-        offset,
-        text: innerText.substring(0, offset),
-      }
-      break;
-    }
-
-    index += innerText.length + 1;
   }
-
-  if (!anchor || !focus)
-    return;
-
-  const isSameNode = anchor.spanNode == focus.spanNode;
-
-  const startRegExp = searchRegExp(isSameNode ? normalizedSearchText : anchor.text, !isSameNode, false);
-  const startText = anchor.textNode.textContent!;
-  anchor.offset = startText.search(startRegExp);
-
-  const endRegExp = searchRegExp(isSameNode ? normalizedSearchText : focus.text, false, !isSameNode);
-  const endText = focus.textNode.textContent!.substring(isSameNode ? anchor.offset : 0);
-  focus.offset = (isSameNode ? anchor.offset : 0) + endText.match(endRegExp)![0].length;
-
-  return [{
-    anchor,
-    focus,
-  }];
 }
 
-function normalizedText(text: string): string {
-  return text.replace(/\s+/g,' ').trim();
-}
 
-function searchRegExp(text: string, start: boolean, end: boolean) {
-  return new RegExp((end ? '^\\s*' : '') + text.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&').replaceAll(' ', '\\s+') + (start ? '\\s*$' : ''));
+function escape(text: string): string {
+  return text.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
 }
 
 export function setSelection(searchText: string): void {
   const root = document.getElementsByClassName('react-pdf__Page__textContent')[0] as HTMLDivElement;
-  const selection = search(searchText, root)?.[0];
-  if (selection) {
-    window.getSelection()!.setBaseAndExtent(selection.anchor.textNode, selection.anchor.offset, selection.focus.spanNode, selection.focus.offset);
+  const searching = search(searchText, root);
+  const { value, done } = searching.next();
+  if (!done) {
+    const { anchor, focus } = value!;
+    const selection = window.getSelection();
+    selection!.setBaseAndExtent(anchor.text, anchor.offset, focus.text, focus.offset);
   }
 }
